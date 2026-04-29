87ee753ae4c61aae07b0958b165957681e6db7e3
Mihai Dogaru <dogarumihai2537@gmail.com>
2026-04-29 22:13:39 +0300

    Style accordions for phone

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 console.log("Hello world!!");
 
 const btnNavMobile = document.getElementById("btnNavMobile");
+const btnAccordionHeader1 = document.getElementById("accordion-header-1");
 
 console.log(btnNavMobile);
 
@@ -9,4 +10,11 @@ btnNavMobile.addEventListener("click", () => {
   if (currentStatus == null) currentStatus = "false";
 
   document.body.dataset.open = currentStatus === "false" ? "true" : "false";
+});
+
+btnAccordionHeader1.addEventListener("click", () => {
+  console.log("Click");
+  const isExpanded =
+    btnAccordionHeader1.getAttribute("aria-expanded") === "true";
+  btnAccordionHeader1.setAttribute("aria-expanded", String(!isExpanded));
 });
